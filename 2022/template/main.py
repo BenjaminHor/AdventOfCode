f2ce3eb0f5ce897395ca4f823d34778e4c8e43b5
Benjamin Hor <benjaminvhor@gmail.com>
2022-12-16 11:19:31 -0500

    Updating template

--- 2022/template/main.py
+++ 2022/template/main.py
@@ -1,4 +1,16 @@
 input_file = "input.prod"  # prod or test
 
 file = open(input_file, "r")
-lines = file.readlines()
+lines = [line.replace("\n", "") for line in file.readlines()]
+
+
+def part_1():
+    return ""
+
+
+def part_2():
+    return ""
+
+
+print("Part 1 Answer: " + part_1())
+print("Part 2 Answer: " + part_2())
